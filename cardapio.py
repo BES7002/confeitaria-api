bb5828d7595cb851b5d9d0d57c7a1bf9d03b52e1
brendo.enzosouza <brendo.enzosouza@gmail.com>
2025-11-19 00:35:56 +0000

    Atualizando

--- cardapio.py
+++ cardapio.py
@@ -10,3 +10,12 @@ def buscar_cardapio():
     ]
 
     return itens
+
+def buscar_por_id(item_id):
+    return{
+        "id": 1,
+        "nome": "Chocomoça",
+        "descrição": "Bolo de chocolate chocantees (para mocas).",
+        "preço": 49.99,
+        "foto": "shop"
+    }
